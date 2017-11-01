b791eac24c5ba75ccb74744b7b0a9e4a1a3f385e
dnguyen22 <don.nguyen22@gmail.com>
2017-11-01 07:15:30 -0500

    Add reformat_subway_dates

--- NY Subway/NY Subway.py
+++ NY Subway/NY Subway.py
@@ -1,6 +1,7 @@
 import pandas
 import pandasql
 import csv
+import datetime
 
 
 def num_rainy_days(filename):
@@ -329,3 +330,23 @@ def time_to_hour(time):
     """
     hour = int(time[:2])
     return hour
+
+
+def reformat_subway_dates(date):
+    """
+    The dates in our subway data are formatted in the format month-day-year.
+    The dates in our weather underground data are formatted year-month-day.
+
+    In order to join these two data sets together, we'll want the dates formatted
+    the same way.  Write a function that takes as its input a date in the MTA Subway
+    data format, and returns a date in the weather underground format.
+
+    Hint:
+    There are a couple of useful functions in the datetime library that will
+    help on this assignment, called strptime and strftime.
+    More info can be seen here and further in the documentation section:
+    http://docs.python.org/2/library/datetime.html#datetime.datetime.strptime
+    """
+    date_formatted = datetime.datetime.strptime(date, '%m-%d-%y')
+    date_formatted = date_formatted.strftime('%Y-%m-%d')
+    return date_formatted
