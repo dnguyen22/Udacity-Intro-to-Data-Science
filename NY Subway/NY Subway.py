1cc06f47ba6757a495fd0d8435b8424248803cff
dnguyen22 <don.nguyen22@gmail.com>
2017-11-01 10:43:52 -0500

    Add entires_histogram to plot data

--- NY Subway/NY Subway.py
+++ NY Subway/NY Subway.py
@@ -2,6 +2,7 @@ import pandas
 import pandasql
 import csv
 import datetime
+import matplotlib.pyplot as plt
 
 
 def num_rainy_days(filename):
@@ -350,3 +351,33 @@ def reformat_subway_dates(date):
     date_formatted = datetime.datetime.strptime(date, '%m-%d-%y')
     date_formatted = date_formatted.strftime('%Y-%m-%d')
     return date_formatted
+
+
+def entries_histogram(turnstile_weather):
+    """
+    Before we perform any analysis, it might be useful to take a
+    look at the data we're hoping to analyze. More specifically, let's
+    examine the hourly entries in our NYC subway data and determine what
+    distribution the data follows. This data is stored in a dataframe
+    called turnstile_weather under the ['ENTRIESn_hourly'] column.
+
+    Let's plot two histograms on the same axes to show hourly
+    entries when raining vs. when not raining. Here's an example on how
+    to plot histograms with pandas and matplotlib:
+    turnstile_weather['column_to_graph'].hist()
+
+    Your histogram may look similar to bar graph in the instructor notes below.
+
+    You can read a bit about using matplotlib and pandas to plot histograms here:
+    http://pandas.pydata.org/pandas-docs/stable/visualization.html#histograms
+
+    You can see the information contained within the turnstile weather data here:
+    https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/turnstile_data_master_with_weather.csv
+    """
+
+    plt.figure()
+    turnstile_weather[turnstile_weather.rain == 0][
+        'ENTRIESn_hourly'].hist()  # your code here to plot a historgram for hourly entries when it is raining
+    turnstile_weather[turnstile_weather.rain == 1][
+        'ENTRIESn_hourly'].hist()  # your code here to plot a historgram for hourly entries when it is not raining
+    return plt
