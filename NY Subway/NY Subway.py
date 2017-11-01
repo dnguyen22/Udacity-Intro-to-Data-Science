1555442eff0716231b5d3e07df65b738a3c2966e
dnguyen22 <don.nguyen22@gmail.com>
2017-11-01 11:01:58 -0500

    Add mann_whitney_plus_means to calculate U statistic, since distribution isn't normal

--- NY Subway/NY Subway.py
+++ NY Subway/NY Subway.py
@@ -3,6 +3,8 @@ import pandasql
 import csv
 import datetime
 import matplotlib.pyplot as plt
+import scipy
+import scipy.stats
 
 
 def num_rainy_days(filename):
@@ -381,3 +383,37 @@ def entries_histogram(turnstile_weather):
     turnstile_weather[turnstile_weather.rain == 1][
         'ENTRIESn_hourly'].hist()  # your code here to plot a historgram for hourly entries when it is not raining
     return plt
+
+
+def mann_whitney_plus_means(turnstile_weather):
+    """
+    This function will consume the turnstile_weather dataframe containing
+    our final turnstile weather data.
+
+    You will want to take the means and run the Mann Whitney U-test on the
+    ENTRIESn_hourly column in the turnstile_weather dataframe.
+
+    This function should return:
+        1) the mean of entries with rain
+        2) the mean of entries without rain
+        3) the Mann-Whitney U-statistic and p-value comparing the number of entries
+           with rain and the number of entries without rain
+
+    You should feel free to use scipy's Mann-Whitney implementation, and you
+    might also find it useful to use numpy's mean function.
+
+    Here are the functions' documentation:
+    http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.mannwhitneyu.html
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
+
+    You can look at the final turnstile weather data at the link below:
+    https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/turnstile_data_master_with_weather.csv
+    """
+    with_rain = turnstile_weather[turnstile_weather.rain == 1]
+    without_rain = turnstile_weather[turnstile_weather.rain == 0]
+
+    with_rain_mean = with_rain['ENTRIESn_hourly'].mean()
+    without_rain_mean = without_rain['ENTRIESn_hourly'].mean()
+    U, p = scipy.stats.mannwhitneyu(with_rain['ENTRIESn_hourly'], without_rain['ENTRIESn_hourly'])
+
+    return with_rain_mean, without_rain_mean, U, p
