ded13b68cb895a4d5e931547130429508a3f2ba8
dnguyen22 <don.nguyen22@gmail.com>
2017-11-02 07:16:51 -0500

    Add MapReduce functions

--- NY Subway/NY Subway.py
+++ NY Subway/NY Subway.py
@@ -7,6 +7,7 @@ import scipy
 import scipy.stats
 import numpy as np
 from ggplot import *
+import sys
 
 
 def num_rainy_days(filename):
@@ -429,8 +430,8 @@ def normalize_features(df):
     sigma = df.std()
 
     if (sigma == 0).any():
-        raise Exception("One or more features had the same value for all samples, and thus could " + \
-                        "not be normalized. Please do not include features with only a single value " + \
+        raise Exception("One or more features had the same value for all samples, and thus could " +
+                        "not be normalized. Please do not include features with only a single value " +
                         "in your model.")
     df_normalized = (df - df.mean()) / df.std()
 
@@ -636,3 +637,79 @@ def plot_weather_data(turnstile_weather):
     plot = ggplot(aes(x='Hour', y='ENTRIESn_hourly', color='rain'), data=turnstile_weather) + geom_bar() + \
         ggtitle('Ridership when Raining') + xlab('Hour') + ylab('Riders')
     return plot
+
+
+def mapper():
+    """
+    The input to this mapper will be the final Subway-MTA dataset, the same as
+    in the previous exercise.  You can check out the csv and its structure below:
+    https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/turnstile_data_master_with_weather.csv
+
+    For each line of input, the mapper output should PRINT (not return) the UNIT as
+    the key, the number of ENTRIESn_hourly as the value, and separate the key and
+    the value by a tab. For example: 'R002\t105105.0'
+
+    Since you are printing the output of your program, printing a debug
+    statement will interfere with the operation of the grader. Instead,
+    use the logging module, which we've configured to log to a file printed
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
+    Note that, unlike print, logging.info will take only a single argument.
+    So logging.info("my message") will work, but logging.info("my","message") will not.
+
+    The logging module can be used to give you more control over your debugging
+    or other messages than you can get by printing them. In this exercise, print
+    statements from your mapper will go to your reducer, and print statements
+    from your reducer will be considered your final output. By contrast, messages
+    logged via the loggers we configured will be saved to two files, one
+    for the mapper and one for the reducer. If you click "Test Run", then we
+    will show the contents of those files once your program has finished running.
+    The logging module also has other capabilities; see
+    https://docs.python.org/2/library/logging.html for more information.
+    """
+
+    for line in sys.stdin:
+        data = line.strip().split(',')
+        if len(data) != 22 or data[5] != 'REGULAR':
+            continue
+        print('{}\t{}'.format(data[1], data[6]))
+
+
+def reducer():
+    """
+    Given the output of the mapper for this exercise, the reducer should PRINT
+    (not return) one line per UNIT along with the total number of ENTRIESn_hourly
+    over the course of May (which is the duration of our data), separated by a tab.
+    An example output row from the reducer might look like this: 'R001\t500625.0'
+
+    You can assume that the input to the reducer is sorted such that all rows
+    corresponding to a particular UNIT are grouped together.
+
+    Since you are printing the output of your program, printing a debug
+    statement will interfere with the operation of the grader. Instead,
+    use the logging module, which we've configured to log to a file printed
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
+    Note that, unlike print, logging.info will take only a single argument.
+    So logging.info("my message") will work, but logging.info("my","message") will not.
+    """
+    last_unit = None
+    entry_count = 0
+
+    for line in sys.stdin:
+        data = line.strip().split('\t')
+
+        if len(data) != 2:
+            continue
+
+        this_unit, count = data
+
+        if last_unit and last_unit != this_unit:
+            print('{}\t{}'.format(last_unit, entry_count))
+            entry_count = 0
+
+        entry_count += float(count)
+        last_unit = this_unit
+
+        if last_unit is not None:
+            print('{}\t{}'.format(last_unit, entry_count))
